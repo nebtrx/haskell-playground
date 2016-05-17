fae900a29dd2f057cfc31cc0527a5236c64a26c9
Omar Ahmed <neb.trx@gmail.com>
2016-05-16 23:03:37 -0500

    Adding delay example

---
+++ stm-delay/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+-- import           Control.Concurrent
+-- import           Control.Concurrent.STM
+-- import           GHC.Conc
+import           System.IO ()
+
+main :: IO ()
+main = putStrLn "Hello"
+--
